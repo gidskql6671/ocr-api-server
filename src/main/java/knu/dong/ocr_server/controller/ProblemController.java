43d49440b03c19a9e3a1ac8b6ace572931f3df80
gidskql6671 <gidskql6671@gmail.com>
2023-05-23 00:24:32 +0900

    mock 컨트롤러 추가

---
+++ src/main/java/knu/dong/ocr_server/controller/ProblemController.java
@@ -0,0 +1,17 @@
+package knu.dong.ocr_server.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/problem")
+@RequiredArgsConstructor
+public class ProblemController {
+
+	@GetMapping
+	public String test(int studentGrade, int problemGrade) {
+		return "test";
+	}
+}
